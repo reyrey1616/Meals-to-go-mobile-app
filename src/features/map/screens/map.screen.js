4daa4cc11779c51ab4fc9b9ac2660529df0f86b3
reyrey1616 <dabboyrey@gmail.com>
2021-06-11 14:09:46 +0800

    Created Favourites Context

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -11,7 +11,7 @@ const Map = styled(MapView)`
 	width: 100%;
 `;
 
-export const MapScreen = () => {
+export const MapScreen = ({ navigation }) => {
 	const { location } = useContext(LocationContext);
 	const { restaurants = [] } = useContext(RestaurantsContext);
 	const [latDelta, setLatDelta] = useState(0);
@@ -45,7 +45,13 @@ export const MapScreen = () => {
 								longitude: restaurant.geometry.location.lng,
 							}}
 						>
-							<Map.Callout>
+							<Map.Callout
+								onPress={() => {
+									navigation.navigate("RestaurantDetail", {
+										restaurant,
+									});
+								}}
+							>
 								<MapCallout restaurant={restaurant} />
 							</Map.Callout>
 						</MapView.Marker>
